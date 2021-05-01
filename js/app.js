3c58bcbc10ef0e170bb642b6e8a5881d41b2af12
Lazarus Musavyi <60751637+LMusavyi@users.noreply.github.com>
2021-04-30 21:55:55 -0400

    javascript complete

--- js/app.js
+++ js/app.js
@@ -1,45 +1,130 @@
 window.onload = () => {
 
     //Means to identify each table element
-    var all = document.querySelectorAll('td');
-    var A = all[0];
-    var B = all[1];
-    var C = all[2];
-    var D = all[3];
+
+    let A = document.querySelectorAll('td')[0];
+    let B = document.querySelectorAll('td')[1];
+    let C = document.querySelectorAll('td')[2];
+    let D = document.querySelectorAll('td')[3];
 
 
 
     //Setting box position defaults
-    var isrotatedA = false;
-    var isrotatedB = false;
-    var isrotatedC = false;
-    var isrotatedD = false;
+    let isrotatedA = false;
+    let isrotatedB = false;
+    let isrotatedC = false;
+    let isrotatedD = false;
 
-    //Event handlers to initiate each rotation and reversion
-    A.addEventListener('click', (RotationA));
-    B.addEventListener('click', (RotationA));
-    C.addEventListener('click', (RotationA));
-    D.addEventListener('click', (RotationA));
-
-    function RotationA(){
-      if(isrotatedA=false){
-        A.classList.add("rotate");
-        isrotatedA=true;
-        B.classList.add("revert");
-        isrotatedB=false;
-        C.classList.add("revert");
-        isrotatedC=false;
-        D.classList.add("revert");
-        isrotatedD=false;
-      }if(isrotatedA=true){
-        A.classList.add("revert");
-        isrotatedA=false;
-        B.classList.add("revert");
-        isrotatedB=false;
-        C.classList.add("revert");
-        isrotatedC=false;
-        D.classList.add("revert");
-        isrotatedD=false;
-      };
-  }
+//Event handlers to initiate each rotation and reversion
+let rotateA = () => {
+  A.style.transform = 'rotate(180deg)';
+  A.style.transition= 'transform .5s';
 }
+
+let revertA = () =>{
+  A.style.transform = 'rotate(0deg)';
+  A.style.transition= 'transform .5s';
+};
+
+
+let rotateB = () => {
+  B.style.transform = 'rotate(180deg)';
+  B.style.transition= 'transform .5s';
+};
+
+let revertB = () =>{
+  B.style.transform = 'rotate(0deg)';
+  B.style.transition= 'transform .5s';
+};
+
+let rotateC = () => {
+  C.style.transform = 'rotate(180deg)';
+  C.style.transition= 'transform .5s';
+};
+
+let revertC = () =>{
+  C.style.transform = 'rotate(0deg)';
+  C.style.transition= 'transform .5s';
+};
+
+
+let rotateD = () => {
+  D.style.transform = 'rotate(180deg)';
+  D.style.transition= 'transform .5s';
+};
+
+let revertD = () =>{
+  D.style.transform = 'rotate(0deg)';
+  D.style.transition= 'transform .5s';
+};
+
+function rotationsA() {
+  if(isrotatedA=false) {
+    rotateA;
+    revertB;
+    revertC;
+    revertD;
+      isrotatedA=true;
+    }else(isrotatedA=true) {
+    revertA;
+    revertB;
+    revertC;
+    revertD;
+
+  isrotatedA=false;
+}
+};
+
+function rotationsB() {
+  if(isrotatedB=false) {
+    rotateA;
+    revertB;
+    revertC;
+    revertD;
+      isrotatedA=true;
+    }else(isrotatedA=true) {
+      revertA;
+      revertB;
+      revertC;
+      revertD;
+  isrotatedB=false;
+}
+};
+
+
+    function rotationsC() {
+        if(isrotatedC=false){
+          rotateC;
+          revertA;
+          revertB;
+          revertD;
+            isrotatedC=true;
+          }if(isrotatedC=true){
+            revertA;
+            revertB;
+            revertC;
+            revertD;
+        isrotatedC=false;
+      }
+        };
+
+        fucntion rotationsD() {
+          if(isrotatedD=false){
+            rotateD;
+            revertB;
+            revertC;
+            revertA;
+              isrotatedD=true;
+            }if(isrotatedA=true){
+              revertA;
+              revertB;
+              revertC;
+              revertD;
+          isrotatedD=false;
+        }
+          };
+A.addEventListener('click',rotationsA());
+B.addEventListener('click',rotationsB);
+C.addEventListener('click',rotationsC);
+D.addEventListener('click',rotationsD);
+};
